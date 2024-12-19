473143a49ae934948ebd8cb4099b8cc2dacaad6e
motti <mottitribal@gmail.com>
2024-12-19 05:43:45 +0200

    cleanup

--- MyProjects/patchSM/myPolyOscWithQuantizeSM/PolyOscSM.cpp
+++ MyProjects/patchSM/myPolyOscWithQuantizeSM/PolyOscSM.cpp
@@ -10,17 +10,12 @@ using namespace daisy;
 using namespace daisysp;
 using namespace patch_sm;
 
-using MyOledDisplay = OledDisplay<SSD130xI2c128x64Driver>;
-
 #define DAC_MAX 4095.f
 #define SCALE_CV 819.2 
 
 DaisyPatchSM patch;
-
 MCP4728 mcp;
 
-MyOledDisplay display;
-uint32_t lastUpdated = 0;
 // Create a random device and seed it
 std::random_device rd;
 std::mt19937 gen(rd()); // Mersenne Twister generator
@@ -30,39 +25,40 @@ std::string waveNames[5];
 int waveform;
 int final_wave;
 float testval;
-
+uint16_t errorCount = 0;
+bool high=false;
 //i2c
 #define BUFF_SIZE 8
 static uint8_t DMA_BUFFER_MEM_SECTION output_buffer[BUFF_SIZE];
-float tone = (1.f/12.f), equalDev = (1.f/7.f);
-GPIO LDac;
+float tone = (1.f/12.f), equalDevision = (1.f/7.f);
 
+//notes vectors of maqams
 std::vector<float> notesVector;
-
 std::vector<float> sika_voltages_one_octave = {0.000f, 0.087f, 0.252f, 0.418f, 0.586f, 0.671f, 0.839f, 1.000f, 1.087f, 1.252f, 1.418f, 1.586f, 1.671f, 1.839f, 2.000f};
 std::vector<float> huseyni_voltages_one_octave = { 0.0, 0.125, 0.25, 0.41666, 0.583, 0.7083, 0.833, 1.0, 1.125, 1.25, 1.41666, 1.583, 1.7083, 1.833, 2.0 };
 std::vector<float> huzam_voltages_one_octave = { 0.0, 3/24.f, 7/24.f, 9/24.f, 15/24.f, 17/24.f, 21/24.f, 1.0, 27/24.f, 31/24.f, 33/24.f, 38/24.f, 41/24.f, 45/24.f, 2.0 };
 std::vector<float> rast_voltages_2_octave = { 0.0, 2.f*tone, 3.5f*tone, 5.f*tone, 7.f*tone, 9.f*tone, 10.5f*tone, 1,1 + 2.f*tone,1 + 3.5f*tone,1 + 5.f*tone,1 + 7.f*tone,1 + 9.f*tone,1 + 10.5f*tone, 2};
 std::vector<float> hijaz_voltages_2_octave = { 0.0, 1.f*tone, 3.5f*tone, 5.f*tone, 7.f*tone, 8.f*tone, 11.f*tone, 1,1 + 1.f*tone,1 + 3.5f*tone,1 + 5.f*tone,1 + 7.f*tone,1 + 8.f*tone,1 + 11.f*tone, 2};
-std::vector<float> equal_Temprament = { 0.0, 1.f*equalDev, 2.f*equalDev, 3.f*equalDev, 4.f*equalDev, 5.f*equalDev, 6.f*equalDev, 1,1 + 1.f*equalDev,1 + 2.f*equalDev,1 + 3.f*equalDev,1 + 4.f*equalDev,1 + 5.f*equalDev,1 + 6.f*equalDev, 2};
+std::vector<float> equal_Temprament = { 0.0, 1.f*equalDevision, 2.f*equalDevision, 3.f*equalDevision, 4.f*equalDevision, 5.f*equalDevision, 6.f*equalDevision, 1,1 + 1.f*equalDevision,1 + 2.f*equalDevision,1 + 3.f*equalDevision,1 + 4.f*equalDevision,1 + 5.f*equalDevision,1 + 6.f*equalDevision, 2};
 std::vector<std::vector<float>> scales_one_octave = { equal_Temprament/*, huzam_voltages_one_octave, rast_voltages_one_octave */};
+
 //knobs
 float cvInArr[2];
-float closestNotes[4] = { 0.0 };
+
+//for oscillator use
 float frequencies[4] = {0};
 
 int getRandomInt(int min, int max);
 void UpdateControls();
 void CalculateClosestNote(float* cvIn, int* indexes);
 uint16_t ConvertNoteToDacValue(float note);
-//testing i2c
+//I2C DAC helpers
 void testWriteMcp(I2CHandle::Config& config);
 I2CHandle::Result WriteMCP_Voltage(I2CHandle& i2c ,uint16_t chA,uint16_t chB);
 void writeVref(I2CHandle& i2c , I2CHandle::Config& config);
 void writeGain(I2CHandle& i2c , I2CHandle::Config& config);
 
-uint16_t errorCount = 0;
-bool high=false;
+//audio callback
 static void AudioCallback(AudioHandle::InputBuffer  in,
                           AudioHandle::OutputBuffer out,
                           size_t                    size)
@@ -79,46 +75,8 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
     //     OUT_L[i]  = sig;
     //     OUT_R[i]  = sig;
     // }
-    // for(size_t i = 0; i < size; i++)
-    // {
-
-         
-    //      float sig = osc[0].Process();
-    //      out[0][i] = sig ;
-    //     // float mix = 0;
-    //     // //Process and output the three oscillators
-    //     // for(size_t chn = 0; chn < 3; chn++)
-    //     // {
-    //     //     float sig = osc[chn].Process();
-    //     //     mix += sig * .25f;
-    //     //     out[chn][i] = sig;
-    //     // }
-
-    //     // //output the mixed oscillators
-    //     // out[3][i] = mix;
-    // }
+   
 }
-
-void SetupOsc(float samplerate)
-{
-    for(int i = 0; i < 3; i++)
-    {
-        osc[i].Init(samplerate);
-        osc[i].SetAmp(.5);
-    }
-}
-
-void SetupWaveNames()
-{
-    waveNames[0] = "sine";
-    waveNames[1] = "triangle";
-    waveNames[2] = "saw";
-    waveNames[3] = "ramp";
-    waveNames[4] = "square";
-}
-
-void UpdateOled();
-
 int main(void)
 {
     notesVector = scales_one_octave.at(0);
@@ -128,37 +86,30 @@ int main(void)
     //patch.StartLog(true);
     samplerate = patch.AudioSampleRate();
 
-    waveform   = 0;
-    final_wave = Oscillator::WAVE_POLYBLEP_TRI;
-
-    SetupOsc(samplerate);
-    SetupWaveNames();
+    //oscillator
+    // waveform   = 0;
+    // final_wave = Oscillator::WAVE_POLYBLEP_TRI;
+    // SetupOsc(samplerate);
+    // SetupWaveNames();
 
     //i2c configuration
-     I2CHandle::Config _i2c_config;
-    _i2c_config.periph = I2CHandle::Config::Peripheral::I2C_1;
-    _i2c_config.speed  = I2CHandle::Config::Speed::I2C_100KHZ;
-    _i2c_config.mode   = I2CHandle::Config::Mode::I2C_MASTER;
-    _i2c_config.address = 0x60;
-    _i2c_config.pin_config.scl  = DaisyPatchSM::B7;
-    _i2c_config.pin_config.sda  =DaisyPatchSM::B8;
-    // initialise the peripheral
-    I2CHandle _i2c;
-    _i2c.Init(_i2c_config);
+    I2CHandle::Config i2c_config;
+    i2c_config.periph = I2CHandle::Config::Peripheral::I2C_1;
+    i2c_config.speed  = I2CHandle::Config::Speed::I2C_100KHZ;
+    i2c_config.mode   = I2CHandle::Config::Mode::I2C_MASTER;
+    i2c_config.address = 0x60;
+    i2c_config.pin_config.scl  = DaisyPatchSM::B7;
+    i2c_config.pin_config.sda  = DaisyPatchSM::B8;
+    // initialise DAC peripheral 
+    I2CHandle i2c_handle;
+    i2c_handle.Init(i2c_config);
     patch.Delay(50);
-    writeVref(_i2c,_i2c_config);
+    writeVref(i2c_handle,i2c_config);
     patch.Delay(50);
-    writeGain(_i2c,_i2c_config);
+    writeGain(i2c_handle,i2c_config);
     patch.Delay(50);
 
     patch.PrintLine("DAC init OK");
-    
-    //oled display configuration
-
-    // MyOledDisplay::Config disp_cfg;
-    // disp_cfg.driver_config.transport_config.i2c_config.pin_config.scl    = DaisyPatchSM::B7;
-    // disp_cfg.driver_config.transport_config.i2c_config.pin_config.sda    = DaisyPatchSM::B8;
-    // display.Init(disp_cfg);
 
     //start
     patch.StartAdc();
@@ -180,10 +131,10 @@ int main(void)
         if(cvOut1 != lastSendChannelA)
         {
             lastSendChannelA = cvOut1;
-            auto i2cResult = WriteMCP_Voltage(_i2c,cvOut1,lastSendChannelB);
+            auto i2cResult = WriteMCP_Voltage(i2c_handle,cvOut1,lastSendChannelB);
             if (i2cResult != I2CHandle::Result::OK) 
             {
-            _i2c.Init(_i2c_config);
+            i2c_handle.Init(i2c_config);
                 //patch.Print("i2c error");
             }
         }
@@ -191,88 +142,23 @@ int main(void)
         if(cvOut2 != lastSendChannelB)
         {
             lastSendChannelB = cvOut2;
-            auto i2cResult = WriteMCP_Voltage(_i2c,lastSendChannelA,cvOut2);
+            auto i2cResult = WriteMCP_Voltage(i2c_handle,lastSendChannelA,cvOut2);
             if (i2cResult != I2CHandle::Result::OK) 
             {
-            _i2c.Init(_i2c_config);
+            i2c_handle.Init(i2c_config);
                 //patch.Print("i2c error");
             }
         }
-  
-
         patch.Delay(2);
-        // auto i2cResult1 =  mcp.analogWrite(MCP4728::DAC_CH::A ,cvOut1 , MCP4728::VREF::INTERNAL_2_8V, MCP4728::PWR_DOWN::NORMAL, MCP4728::GAIN::X1);
-        // patch.Delay(20);
-        // auto i2cResult2 =  mcp.analogWrite(MCP4728::DAC_CH::B ,cvOut2 , MCP4728::VREF::INTERNAL_2_8V, MCP4728::PWR_DOWN::NORMAL, MCP4728::GAIN::X1);
-        // patch.Delay(20);
-        // if (i2cResult1 != I2CHandle::Result::OK || i2cResult2 != I2CHandle::Result::OK) 
-        // {
-        //     mcp.Init(i2c_conf);
-        //     patch.Print("i2c error count");
-        // }
-
-        //////////////////////////////////////////////
-        // patch.Delay(50);
-        // auto i2cResult = WriteMCP_Voltage(_i2c,4095,4095);
-        // if (i2cResult != I2CHandle::Result::OK) 
-        // {
-        //    _i2c.Init(_i2c_config);
-        //     patch.Print("i2c error");
-        // }
-        // patch.Delay(50);
-        // i2cResult = WriteMCP_Voltage(_i2c,0,0);
-        // if (i2cResult != I2CHandle::Result::OK) 
-        // {
-        //     _i2c.Init(_i2c_config);
-        //     patch.Print("i2c error");
-        // }
-        //UpdateOled();
         
     }
 }
 
-void UpdateOled()
-{
-    u_int32_t now = System::GetNow();
-    if(now - lastUpdated > 100)
-    {
-        lastUpdated = now;
-        char strbuff[24];
-        sprintf(strbuff, "welcome 3 daisy!");
-        display.Fill(false);
-        display.SetCursor(0, 0);
-        display.WriteString(strbuff, Font_7x10, true);
-        display.SetCursor(0, 11);
 
-        // std::string str = std::to_string(cvInArr[0]);
-        // const int length = str.length(); 
-        // char* char_array = new char[length + 1]; 
-        // strcpy(char_array, str.c_str()); 
-        // display.WriteString(char_array, Font_7x10, true);
-       
-        int randomX = getRandomInt(0,128);
-
-        int randomY = getRandomInt(15,64);
-        display.DrawCircle(randomX, randomY,5,true);
-        display.Update();
-    }
-    
-}
-
-void UpdateControls()
-{
-    patch.ProcessAllControls();
-
-    osc[0].SetFreq(frequencies[0]);
-    osc[0].SetWaveform(Oscillator::WAVE_SAW);
-
-}
 
 void CalculateClosestNote(float* cvIn, int* indexes){
     float minDiff1 = cvIn[0], minDiff2 = cvIn[1];
-    closestNotes[0] = 0;
-    closestNotes[1] = 0;
-    //int index1 =0,index2 = 0;
+
     for (auto index = 0; index < notesVector.size(); ++index) 
     {
         float diff1 = std::abs(notesVector[index] - cvIn[0]), diff2 = std::abs(notesVector[index] - cvIn[1]);
@@ -296,6 +182,12 @@ uint16_t ConvertNoteToDacValue(float note){
     return (note == 2.000f) ? (uint16_t)(note*1948+80) : (uint16_t)(note*2000);
 }
 
+void UpdateControls()
+{
+    patch.ProcessAllControls();
+    osc[0].SetFreq(frequencies[0]);
+    osc[0].SetWaveform(Oscillator::WAVE_SAW);
+}
 
 int getRandomInt(int min, int max) {
 
@@ -368,3 +260,20 @@ void writeGain(I2CHandle& i2c , I2CHandle::Config& config){
 
 }
         
+void SetupOsc(float samplerate)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        osc[i].Init(samplerate);
+        osc[i].SetAmp(.5);
+    }
+}
+
+void SetupWaveNames()
+{
+    waveNames[0] = "sine";
+    waveNames[1] = "triangle";
+    waveNames[2] = "saw";
+    waveNames[3] = "ramp";
+    waveNames[4] = "square";
+}
